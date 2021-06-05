5ac5589b2d0a817da84252fae40b64c604dcd8c7
Elias.Acevedo <60749227+EliasAcevedoUTN@users.noreply.github.com>
2021-06-05 18:00:24 -0300

    comprimi tpcliente vue por problemas con github

--- tpclientevue/backend/src/mysqldb.ts
+++
@@ -1,9 +0,0 @@
-import {createPool} from 'mysql';
-
-export const conexionMysql = createPool ({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'instrumentosdb',
-    connectionLimit: 100 //100 es el valor por defecto
-})
